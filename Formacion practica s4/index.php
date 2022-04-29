e660223e0a65c4dcfd6f26c984ec6d7e3dafa4e3
danivto247 <68718330+danivto247@users.noreply.github.com>
2022-04-29 08:59:53 -0500

    Update index.php

--- Formacion practica s4/index.php
+++ Formacion practica s4/index.php
@@ -1,4 +1,7 @@
 <?php
+    #conexion a la base de datos
+    require_once '3_modelo/conexion.php';
+    conectar();
     #mostrar la plantilla y el resultado
 
     require_once '2_controlador/controlador.php';
