8d74976e7f0329314c03d7cb13bf87953be14662
danivto247 <68718330+danivto247@users.noreply.github.com>
2022-06-21 18:39:14 -0500

    Delete Formacion practica s4 directory

--- Formacion practica s4/3_modelo/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function conectar();
-        $user="id18812490_root"
-        $pass="|b/0&K[X]QegN<}&"
-        $server="localhost"
-        $db="id18812490_app"
-        $con=mysqli_connect($server,$user,$pass) or die ("error al conectar a la base de datos".mysql_error());
-        mysqli_select_db('id18812490_app', $con)
-?>
